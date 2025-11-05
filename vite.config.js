7a2f204a9b00e2eab46d0f74b379dc955649b5ab
Kart220 <karthicksriyuvanraj@gmail.com>
2025-11-05 22:32:43 +0530

    chore: fix vite base, add vite.svg, add gh-pages deploy script

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/Kart220'
-  plugins: [react()],
+ base: '/Kart220/',
+plugins: [react()],
   server: {
     port: 3000,
   },
-})
+})  
